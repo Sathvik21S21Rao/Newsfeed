3b59b6ad0a78e8e5a3dd7a46b3936b63aa0ebd85
Sathvik S Rao <sathvikrao04@gmail.com>
2021-05-07 16:37:27 +0530

    7-5-2021

--- Project/News/urls.py
+++ Project/News/urls.py
@@ -1,3 +1,4 @@
 from django.urls import path
 from . import views
-urlpatterns=[ path('',views.response,name='response'),path("sign_up",views.sign_up,name="sign_up")]
+urlpatterns=[ path('',views.response,name='response'),path("sign_up",views.sign_up,name="sign_up"),
+path("genres",views.genres,name="genres")]
--- Project/News/views.py
+++ Project/News/views.py
@@ -3,4 +3,6 @@ def response(request):
     return render(request,'login.html')
 def sign_up(request):
     return render(request,'sign_up.html')
+def genres(request):
+    return render(request,'genres.html')
 # Create your views here.
