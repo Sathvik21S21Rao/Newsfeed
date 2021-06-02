2e5acd9e9f17b4414c03f2e6246b22eb1741b6f2
Sathvik S Rao <sathvikrao04@gmail.com>
2021-06-02 22:48:47 +0530

    Change Password

--- Project/News/views.py
+++ Project/News/views.py
@@ -3,10 +3,41 @@ from django.contrib import messages
 import requests
 from django.contrib.auth.models import User,auth
 from .models import Userverification as userinfo
-USER=''
-def emailverifier(email):
-        
+
+import string
+USER=[]
+def password_check(passwd):
+    SpecialSym =string.punctuation
+    val = True
+      
+    if len(passwd) < 6:
+        print('length should be at least 6')
+        val = False
+          
+    if len(passwd) > 20:
+        print('length should be not be greater than 8')
+        val = False
+          
+    if not any(char.isdigit() for char in passwd):
+        print('Password should have at least one numeral')
+        val = False
+          
+    if not any(char.isupper() for char in passwd):
+        print('Password should have at least one uppercase letter')
+        val = False
+          
+    if not any(char.islower() for char in passwd):
+        print('Password should have at least one lowercase letter')
+        val = False
+          
+    if not any(char in SpecialSym for char in passwd):
+        print('Password should have at least one of the symbols $@#')
+        val = False
     
+    return val
+  
+
+def emailverifier(email):
     response = requests.get(
         "https://isitarealemail.com/api/email/validate",
         params = {'email': email})
@@ -26,7 +57,6 @@ def login(request):
         user= auth.authenticate(username=username,password=password)
         if user:
             auth.login(request,user)
-
             return render(request,"home.html")
         elif not User.objects.filter(username=username).exists():
             messages.error(request,"Username does not exist")
@@ -35,21 +65,24 @@ def login(request):
             messages.error(request,"Password is incorrect")
             return redirect("/")
     except:
-        d={}
-        L=['Sports','Business','Health','Entertainment','Science','Technology','Nation']
-        for i in L:
-            try:
-                request.POST[i]
-                d[i]=True
-            except:
-                d[i]=False
-        genres=""
-        for i in d:
-            if d[i]:
-                genres+=i+" "
-        USER+=[genres]
-        k=userinfo(uname=USER[0],location=USER[1],genres=USER[2])  
-        k.save()  
+        try:
+            d={}
+            L=['Sports','Business','Health','Entertainment','Science','Technology','Nation']
+            for i in L:
+                try:
+                    request.POST[i]
+                    d[i]=True
+                except:
+                    d[i]=False
+            genres=""
+            for i in d:
+                if d[i]:
+                    genres+=i+" "
+            USER+=[genres]
+            k=userinfo(uname=USER[0],location=USER[1],genres=USER[2])  
+            k.save()  
+        except:
+            pass
         return render(request,"home.html")
 
 def sign_up(request):
@@ -66,7 +99,9 @@ def genres(request):
         if password!=confirm:
             messages.error(request,'Password does not match confirm')
             return redirect('/sign_up')
-
+        if not password_check(password):
+            messages.error(request,"Enter a stronger password")
+            return redirect('/sign_up')
         elif emailverifier(email)==False:
             messages.error(request,email+" does not exist")
             return redirect('/sign_up')
@@ -88,5 +123,30 @@ def genres(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
+def changepass(request):
+    if request.method=="POST":
+        current=request.POST["cpass"]
+        new=request.POST["newpass"]
+        confirm=request.POST["confnewpass"]
+        if new!=confirm:
+            messages.error(request,"Confirm and Password not matching")
+            return redirect("/changepass")
+        user=User.objects.get(id=request.user.id)
+        check=user.check_password(current)
+        if check:
+            if password_check(new):
+                user.set_password(new)
+                user.save()
+                messages.success(request,"Succesfully changed the password")
+                return redirect("/home")
+            else:
+                messages.error(request,"Password conditions not met")
+                return redirect("/changepass")
+        else:
+            messages.error(request,"Wrong password")
+            return redirect("/changepass")
+
+
+    return render(request,"changepass.html")
 
 # Create your views here.
